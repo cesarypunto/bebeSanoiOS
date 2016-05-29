d91aaf2efad691c5bf099096e2881ef4365b2935
César Gutiérrez <gt.cesar@gmail.com>
2016-05-29 02:14:09 +0200

    calendar

--- BebeSano/ViewController.swift
+++ BebeSano/ViewController.swift
@@ -10,14 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+        
+    @IBOutlet weak var dateTextField: UITextField!
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //datePicker.addTarget(self, action: Selector("dataPickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func dateFieldEditing(sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: #selector(ViewController.datePickerValueChanged),forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        /*dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle*/
+       
+        
+        let formatter = NSDateFormatter()
+        let gbDateFormat = NSDateFormatter.dateFormatFromTemplate("MMddyyyy", options: 0, locale: NSLocale(localeIdentifier: "es-ES"))
+        
+        
+        formatter.dateFormat = gbDateFormat
+        
+        dateTextField.text = formatter.stringFromDate(sender.date)
+        
     }
 
 
